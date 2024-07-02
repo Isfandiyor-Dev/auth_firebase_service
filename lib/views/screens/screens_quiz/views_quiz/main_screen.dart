21b3a2b217425a79eab846ce302e7265e6319479
Isfandiyor-Dev <hello.world01110010@gmail.com>
2024-07-02 08:27:23 +0500

    commit

--- lib/views/screens/screens_quiz/views_quiz/main_screen.dart
+++ lib/views/screens/screens_quiz/views_quiz/main_screen.dart
@@ -1,14 +1,45 @@
+import 'package:auth_firebase_service/controllers/auth_controller.dart';
+import 'package:auth_firebase_service/utils/messages.dart';
+import 'package:auth_firebase_service/utils/routes.dart';
 import 'package:auth_firebase_service/views/screens/screens_quiz/views_quiz/custom_quiz_manage/custom_quizzes.dart';
 import 'package:auth_firebase_service/views/screens/screens_quiz/views_quiz/quiz_game.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  void onTapLogOut() {
+    Messages.showLoadingDialog(context);
+    Provider.of<AuthController>(
+      context,
+      listen: false,
+    ).logOut().then((value) {
+      Navigator.pop(context);
+      Navigator.pushReplacementNamed(context, AppRoutes.login);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primaryFixedDim,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+            onPressed: onTapLogOut,
+            icon: const Icon(
+              Icons.logout_rounded,
+            ),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
